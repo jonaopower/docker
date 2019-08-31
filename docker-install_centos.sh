47832ba50da8090512b80c5970d240b7531b7a4a
root <jonas.soares@gmail.com>
2019-08-30 20:54:06 -0400

    Atualizando para meetup v2

---
+++ docker-install_centos.sh
@@ -0,0 +1,5 @@
+curl -fsSL https://get.docker.com | bash
+systemctl start docker
+systemctl enable docker
+yum install -y docker-compose git
+
---
+++ docker-install_ubuntu.sh
@@ -0,0 +1,5 @@
+curl -fsSL https://get.docker.com | bash
+systemctl start docker
+systemctl enable docker
+apt-get install -y docker-compose git
+
